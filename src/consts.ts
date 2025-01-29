1e68833a74cc10a85976357b5e2b4be2536f8494
shay <32275182+404shay@users.noreply.github.com>
2025-01-29 11:45:28 -0500

    init commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Shay Paris';
+export const SITE_DESCRIPTION = 'Shay Paris is an engineer specialising in messaging systems and abuse prevention.';
